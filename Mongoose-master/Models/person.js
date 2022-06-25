8e55a730c857ce0c02104c5f7f6348ace0b1e90c
chaker95 <95969351+chaker95@users.noreply.github.com>
2022-06-25 15:29:03 +0100

    Delete Mongoose-master directory

--- Mongoose-master/Models/person.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const schema = mongoose.Schema;
-
-const personSchema = new schema({
-    name: { type: String, required: true },
-    age: Number,
-    favoriteFoods: [String],
-});
-module.exports = mongoose.model("Person", personSchema);
